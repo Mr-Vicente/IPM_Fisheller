946e0ba96be2c4afce8df766aa8f30df58c32502
Mariana Bonito <mariana.f.bonito@gmail.com>
2020-11-25 22:58:42 +0000

    Book Page plus minor adjustments on fish and tips

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fisheller_app/constants.dart';
 import 'package:fisheller_app/screens/auth/login/login_screen.dart';
 import 'package:fisheller_app/screens/auth/welcome/welcome_screen.dart';
+import 'package:fisheller_app/screens/book_fish/book_screen.dart';
 import 'package:fisheller_app/screens/market/temp.dart';
 import 'package:flutter/material.dart';
 import 'package:fisheller_app/screens/map/map_screen.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
     josefinaBass.market = docaPortimao_market;
     josefinaLobster.market = docaPortimao_market;
     josefinaCod.market = docaPortimao_market;
-
+    seabass2.media = seabassMedia;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -35,7 +36,9 @@ class MyApp extends StatelessWidget {
       //home: MapPage(),
       //home: Home(),
       //home: Temp(),
-      home:FishAndTips(lobsterTips)
+      //home:FishAndTips(codTips),
+      //home:FishAndTips(lobsterTips),
+      home: BookFish(seabass2, 'Doca Portimão')
     );
   }
 }
